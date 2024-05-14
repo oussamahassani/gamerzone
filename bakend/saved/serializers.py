aa8ab0c0a13c9d29e4e6a1b17e320337d3ddeae0
emna0503 <emnamallek9@gmail.com>
2024-05-14 20:08:35 +0100

    saved post modif

--- bakend/saved/serializers.py
+++ bakend/saved/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
+
+from posts.serializers import PostSerializer
+from users.serializers import CustomUserSerializer
 from .models import SavedPost
 
 class SavedPostSerializer(serializers.ModelSerializer):
+    post = PostSerializer()
+    user =  CustomUserSerializer()
+
     class Meta:
         model = SavedPost
         fields = '__all__'
