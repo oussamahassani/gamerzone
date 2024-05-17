3fcaeab273a4471ae0535657283ef94c0dc429e3
oussamahassani <oussamahassanisimplon@gmail.com>
2024-05-17 01:30:56 +0100

    add group if have folower

--- frontend/src/components/Groups/index.js
+++ frontend/src/components/Groups/index.js
@@ -1,7 +1,9 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState,useEffect } from "react";
 import Button from '@material-ui/core/Button';
 import AddGroup from "./actions/add_group";
 import GroupLayout from "./Group_layout"
+import axios from 'axios';
+
 export default function Post() {
     const BASE_URL_HTTP = process.env.REACT_APP_BASE_URL_HTPP;
     const parameters = {
@@ -9,17 +11,51 @@ export default function Post() {
         url: `${BASE_URL_HTTP}/posts/profile_post/ `
     }
     const [OpenAddPost, setOpenAddPost] = useState(false);
-    const [OpenAddStory, setOpenAddStory] = useState(false)
+    const [mydata, setmydata] = useState(null)
+    const [follower, setfollowers] = useState([])
+    
     const handleCloseAddPost = () => {
         setOpenAddPost(false);
 
     };
+useEffect(() => {
+  
+
+
+
+    const x = localStorage.getItem('token');
+    axios.get(`${BASE_URL_HTTP}/user/findCurrent`, {
+      headers: {
+        'Authorization': `token ${x}`,
+      },
+
+    }).then((res) => {
+      setmydata(res.data)
+      axios.get(`${BASE_URL_HTTP}/user/followers_followings/${res.data.user_name}`, {
+        headers: {
+          'Authorization': `token ${x}`,
+
+        },
+        params: {
+          type: 'followers',
+
+        }
+      }).then((res) => {
+        setfollowers(res.data.followers);
+
+      })
+    }
+      , (error) => { console.log(error.message, error.response) })
+      return () => {
+    
+      }
+    }, [])
     return (
         <>
             <div style={{ border: 'none' }}>
-                <Button variant="outlined" style={{ marginBottom: '20px', marginTop: '20px' }} onClick={() => { setOpenAddPost(true) }}>Add News groups</Button>
+                {follower.length >= 4 && <Button variant="outlined" style={{ marginBottom: '20px', marginTop: '20px' }} onClick={() => { setOpenAddPost(true) }}>Add News groups</Button>}
 
-            </div>        {OpenAddPost && <AddGroup open={OpenAddPost} onClose={handleCloseAddPost} />}
+            </div>        { OpenAddPost && <AddGroup open={OpenAddPost} onClose={handleCloseAddPost} />}
 
             <GroupLayout />
         </>
